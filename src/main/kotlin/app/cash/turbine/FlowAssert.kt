341bb066c0cffb353f7e175ac9c1de6d4e9834d9
Jake Wharton <jw@squareup.com>
2020-07-29 12:30:57 -0400

    Set up basic build and repo structure

--- src/main/kotlin/app/cash/turbine/FlowAssert.kt
+++ src/main/kotlin/app/cash/turbine/FlowAssert.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.squareup.sqldelight.runtime.coroutines
+package app.cash.turbine
 
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Job
