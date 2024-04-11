4214a39881a11585a23d5e4f513aad1ddd52409e
Jake Wharton <jw@squareup.com>
2024-04-10 20:54:49 -0400

    Update channel tests
    Do not use internal functions to create channels.

--- src/jvmTest/kotlin/app/cash/turbine/ChannelJvmTest.kt
+++ src/jvmTest/kotlin/app/cash/turbine/ChannelJvmTest.kt
@@ -2,7 +2,6 @@ package app.cash.turbine
 
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -10,7 +9,7 @@ class ChannelJvmTest {
   @Test
   fun takeItemSuspendingThrows() = runTest {
     val actual = assertFailsWith<IllegalStateException> {
-      emptyFlow<Unit>().collectIntoChannel(this).takeItem()
+      emptyChannel().takeItem()
     }
     assertEquals("Calling context is suspending; use a suspending method instead", actual.message)
   }
